5ee1f1c98b5468f030141d221eca070c1aed284d
cybercruzer <88536975+cybercruzer@users.noreply.github.com>
2024-01-30 15:23:38 +0530

    ignor
    ignor

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,7 +3,9 @@ import resList from "../utils/mockData"
 
 const Body = () => (
     <div className="body">
-        <div className="search">search</div>
+        <div className="filter">
+            <button className="filter-btn" onClick={ () => {console.log("buttonclickedddd")} } >top rated </button>   
+        </div>
         <div className="rest-container">
           {
           resList.map(restaurant => <RestCard key={restaurant.data.id} resData={restaurant}/>)
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-export const CDN_URL = "https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg";
+export const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const LOGO_URL = "https://cdn.vectorstock.com/i/preview-1x/73/82/food-logo-vector-38377382.jpg";
 
